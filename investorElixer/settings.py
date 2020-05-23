0c66e6d52e085accb3cb17adf64f7c91eeae2247
tk125 <tsukian125@gamil.com>
2020-05-23 21:34:28 +0800

    DB creation

--- investorElixer/settings.py
+++ investorElixer/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'investorapp',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +75,16 @@ WSGI_APPLICATION = 'investorElixer.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': {        
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'investorelixer',
+        'USER': 'root',
+        'PASSWORD': 'root123!',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
+
 
 
 # Password validation
---
+++ investorapp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Stock(models.Model):
+
+    name = models.CharField(max_length=100)
+    ticker = models.CharField(max_length=10)
+    closing_price = models.DecimalField(decimal_places=2,max_digits=10)
+
+class Broker(models.Model):
+
+    name = models.CharField(max_length=100)
+    minimum_fee = models.DecimalField(decimal_places=2,max_digits=10)
+    trading_fee = models.DecimalField(decimal_places=2,max_digits=10)
+
+class Transaction(models.Model):
+
+    order_type = models.PositiveSmallIntegerField()
+    price = models.DecimalField(decimal_places=2,max_digits=10)
+    quantity = models.IntegerField()
+    txn_date = models.IntegerField()
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    broker = models.ForeignKey(Broker, on_delete=models.CASCADE)
